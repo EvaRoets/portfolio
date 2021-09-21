240113cf4851cefda279cbdc04df3c4a5d2430c7
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-21 15:39:05 +0200

    Install FontAwesome for React

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
+// import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import fas from '@fortawesome/fontawesome-free-solid';
+import far from '@fortawesome/fontawesome-free-regular';
+import fab from '@fortawesome/fontawesome-free-brands';
+import { faMobileAlt, faEnvelopeOpen, faCopy } from '@fortawesome/free-solid-svg-icons' ;
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, fab, far, faMobileAlt, faEnvelopeOpen, faCopy, faLinkedin, faGithub);
 
 
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,34 @@
-import React from "react"
+import React from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-function Contact(props) {
+
+function Contact() {
     return (
-        <div class="contact" id="contact">
+        <div className="contact" id="contact">
             <h1>Contact</h1>
             <p>
-                <i className="fas fa-mobile-alt"></i>
+                <FontAwesomeIcon icon="mobile-alt"/>
                 <a href="tel:+32493140480">+32 493 140 480</a>
             </p>
 
             <div className="contact-email-container">
                 <p id="email">
-
-                    <i className="far fa-envelope-open"></i>
+                    <FontAwesomeIcon icon={['far', 'envelope-open']}/>
                     <a href="mailto:eva_roets@hotmail">eva_roets@hotmail</a>
                 </p>
 
                 <button className="copy-email-btn tooltip" onClick="CopyToClipboard('email')">
-                    <i className="fas fa-copy"></i
-                    ><span className="tooltiptext">copy email</span>
+                    <FontAwesomeIcon icon={['far', 'copy']}/>
+                    <span className="tooltiptext"></span>
                 </button>
             </div>
 
-            <a href="https://www.linkedin.com/in/evaroets/" target="_blank">
-                <i className="fab fa-linkedin-in"></i>
+            <a href="https://www.linkedin.com/in/evaroets/" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={['fab', 'linkedin-in']}/>
                 https://www.linkedin.com/in/evaroets/</a>
 
-            <a href="https://github.com/EvaRoets" target="_blank">
-                <i className="fab fa-github"></i>
+            <a href="https://github.com/EvaRoets" target="_blank" rel="noreferrer">
+                <FontAwesomeIcon icon={['fab', 'github']}/>
                 https://github.com/EvaRoets</a>
         </div>
     )
