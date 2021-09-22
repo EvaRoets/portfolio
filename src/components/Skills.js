87d00d84e3973d0fd925470944470eae4d6b8e78
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-22 12:32:14 +0200

    Add skills table

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@ import React from "react"
 
 // import fontawesome icons
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Table} from "react-bootstrap";
 import GoToTop from "./GoToTop";
 
 
@@ -19,6 +19,39 @@ function Skills() {
                         <div className="skills-text">
                             <p>These are the skills and technologies I know how to use or wish to specialize in further.</p>
                         </div>
+                        <Col md={{span: 8, offset: 2}} className="skills-table">
+                            <Table   hover>
+                                <tbody>
+                                <tr>
+                                    <td>HTML/CSS</td>
+                                    <td>PHP</td>
+                                    <td>Indesign</td>
+                                    <td>WordPress</td>
+
+                                </tr>
+                                <tr>
+                                    <td>JavaScript</td>
+                                    <td>Laravel</td>
+                                    <td>Illustrator</td>
+                                    <td>SEO/SEA</td>
+
+                                </tr>
+                                <tr>
+                                    <td>React</td>
+                                    <td>GIT</td>
+                                    <td>PhotoShop</td>
+                                    <td>Digital Marketing</td>
+
+
+
+                                </tr>
+                                </tbody>
+                            </Table>
+                        </Col>
+
+                        {/*<div className="skills-table">*/}
+                        {/*   */}
+                        {/*</div>*/}
                     </Col>
                 </Row>
             </Container>
