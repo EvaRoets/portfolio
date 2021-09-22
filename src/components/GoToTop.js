1e46dece88ab2786a7174b3171f0094cc154d1ae
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-22 10:20:58 +0200

    Replace a tag with GoToTop component

--- src/components/GoToTop.js
+++ src/components/GoToTop.js
@@ -2,14 +2,13 @@ import React from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Container, Row, Col} from "react-bootstrap";
 
-
 const GoToTop = () => {
     return (
         <>
-            <Container fluid="lg" className="footer sticky">
+            <Container fluid="lg">
                 <Row>
                     <Col md={{span: 8, offset: 2}} className="text-center">
-                        <a href="#top">
+                        <a href="#top" id="top">
                             <FontAwesomeIcon icon="chevron-circle-up"/>
                             Let's go back to the top
                         </a>
