ee920d1c6a163955d4f05c5f466ebd97a3925e73
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-10-04 15:33:39 +0200

    Refactor About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,73 +1,31 @@
-import React from "react"
+import React from "react";
+import GoToTop from "./GoToTop";
+
+// import Bootstrap elements
+import {Container, Row, Col} from "react-bootstrap";
 
 // import fontawesome icons
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {Container, Row, Col} from "react-bootstrap";
-import GoToTop from "./GoToTop";
 
-function About() {
+const About = () => {
     return (
         <>
-            <Container fluid="lg" class="about-me" id="about-me">
+            <Container fluid="lg" class="about-container" id="about">
                 <Row className="justify-content-sm-center-center">
                     <Col md={{span: 10, offset: 1}} className="text-center">
                         <div className="containerIcon">
                             <FontAwesomeIcon icon={['far', 'user']} size="3x" color="red"/>
                         </div>
                         <h2>About me</h2>
-
                         <div className="about-text">
-                            <p> I started off in communication and digital marketing and got a taste for web development. On
-                                the exciting adventure
-                                of learning the ropes.</p>
+                            <p> I started off in communication and digital marketing and got a taste for web
+                                development. On the exciting adventure of learning the ropes.</p>
                             <p>Wat is Lorem Ipsum?
-                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de
-                                standaard proeftekst in
-                                deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
-                                en ze door elkaar
-                                husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is
-                                ook, vrijwel
-                                onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair
-                                geworden met de introductie
-                                van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing
-                                software zoals Aldus
-                                PageMaker die versies van Lorem Ipsum bevatten.</p>
-                            <p>Wat is Lorem Ipsum?
-                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de
-                                standaard proeftekst in
-                                deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
-                                en ze door elkaar
-                                husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is
-                                ook, vrijwel
-                                onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair
-                                geworden met de introductie
-                                van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing
-                                software zoals Aldus
-                                PageMaker die versies van Lorem Ipsum bevatten.</p>
-                            <p>Wat is Lorem Ipsum?
-                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de
-                                standaard proeftekst in
-                                deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
-                                en ze door elkaar
-                                husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is
-                                ook, vrijwel
-                                onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair
-                                geworden met de introductie
-                                van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing
-                                software zoals Aldus
-                                PageMaker die versies van Lorem Ipsum bevatten.</p>
-                            <p>Wat is Lorem Ipsum?
-                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de
-                                standaard proeftekst in
-                                deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters nam
-                                en ze door elkaar
-                                husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is
-                                ook, vrijwel
-                                onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair
-                                geworden met de introductie
-                                van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing
-                                software zoals Aldus
-                                PageMaker die versies van Lorem Ipsum bevatten.</p>
+                                Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum
+                                is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters
+                                nam en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd
+                                maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair
+                                geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.</p>
                         </div>
                     </Col>
                 </Row>
