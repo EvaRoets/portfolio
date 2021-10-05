f6c0cd55af968354da1c0c681d8e59e0f076d6b8
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-10-05 09:48:46 +0200

    Add styling to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,10 @@
 import React from "react";
 import GoToTop from "./GoToTop";
 
+import Weather from "../css/images/weather-app.PNG"
+import ToDo from "../css/images/to-do-list.png"
+
+
 // import Bootstrap elements
 import {Container, Row, Col, Card, Button, Badge} from "react-bootstrap";
 
@@ -17,60 +21,78 @@ const Projects = () => {
                         <div className="containerIcon">
                             <FontAwesomeIcon icon="code" size="3x" color="#79B4B7"/>
                         </div>
-                        <h2>Apps I have built</h2>
+                        <h2>Projects</h2>
+                        <div className="projects-text">
+                            <p>Would you like to get a better idea of my work? <br/> Here are some of my star projects.
+                            </p>
+                        </div>
                         <div className="projects-gallery">
                             <Card id="project-one">
-                                <Card.Img variant="top" src=""/>
                                 <Card.Body>
                                     <Card.Title>A Weather App</Card.Title>
-                                    <h6>
-                                        <Badge bg="muted">CSS</Badge>
-                                        <Badge bg="muted">JS</Badge>
-                                        <Badge bg="muted">API</Badge>
-                                    </h6>
+                                    <div className="card-badge">
+                                        <h6>
+                                            <Badge bg="muted">CSS</Badge>
+                                            <Badge bg="muted">JS</Badge>
+                                            <Badge bg="muted">API</Badge>
+                                        </h6>
+                                    </div>
+                                    {/*<Card.Img variant="top" src={Weather}/>*/}
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button>Inspect the code</Button>
-                                    {/*https://github.com/EvaRoets/weather-app*/}
-                                    <Button>Check out the project</Button>
-                                    {/*https://affectionate-wilson-13a015.netlify.app/*/}
+                                    <div className="card-button">
+                                        <Button>See code</Button>
+                                        {/*https://github.com/EvaRoets/weather-app*/}
+                                        <Button>See Result</Button>
+                                        {/*https://affectionate-wilson-13a015.netlify.app/*/}
+                                    </div>
+
                                 </Card.Body>
                             </Card>
                             <Card id="project-two">
-                                <Card.Img variant="top" src=""/>
                                 <Card.Body>
-                                    <Card.Title>Your to-do list</Card.Title>
-                                    <h6>
-                                        <Badge bg="muted">CSS</Badge>
-                                        <Badge bg="muted">JS</Badge>
-                                        <Badge bg="muted">React</Badge>
-                                    </h6><Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary">Inspect the code</Button>
-                                    {/*https://github.com/EvaRoets/intro-react*/}
-                                    <Button variant="primary">Check out the project</Button>
-                                    {/*https://competent-curie-7ba046.netlify.app/*/}
-                                </Card.Body>
-                            </Card>
-                            <Card id="project-three">
-                                <Card.Img variant="top" src=""/>
-                                <Card.Body>
-                                    <Card.Title>Order form</Card.Title>
-                                    <h6>
-                                        <Badge bg="muted">CSS</Badge>
-                                        <Badge bg="muted">PHP</Badge>
-                                    </h6>
+                                    <Card.Title>A to-do list</Card.Title>
+                                    <div className="card-badge">
+                                        <h6>
+                                            <Badge bg="muted">CSS</Badge>
+                                            <Badge bg="muted">JS</Badge>
+                                            <Badge bg="muted">React</Badge>
+                                        </h6>
+                                    </div>
+                                    {/*<Card.Img variant="top" src={ToDo}/>*/}
                                     <Card.Text>
                                         Some quick example text to build on the card title and make up the bulk of
                                         the card's content.
                                     </Card.Text>
-                                    <Button variant="primary">Inspect the code</Button>
-                                    {/*https://github.com/EvaRoets/order-form*/}
-                                    <Button variant="primary">Check out the project</Button>
+                                    <div className="card-button">
+                                        <Button>See code</Button>
+                                        {/*https://github.com/EvaRoets/intro-react*/}
+                                        <Button>See result</Button>
+                                        {/*https://competent-curie-7ba046.netlify.app/*/}
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                            <Card id="project-three">
+                                <Card.Body>
+                                    <Card.Title>An order form</Card.Title>
+                                    <div className="card-badge">
+                                        <h6>
+                                            <Badge bg="muted">CSS</Badge>
+                                            <Badge bg="muted">PHP</Badge>
+                                        </h6>
+                                    </div>
+                                    {/*<Card.Img variant="top" src={ToDo}/>*/}
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                    </Card.Text>
+                                    <div className="card-button">
+                                        <Button>See code</Button>
+                                        {/*https://github.com/EvaRoets/order-form*/}
+                                        <Button>See result</Button>
+                                    </div>
                                 </Card.Body>
                             </Card>
                         </div>
